ff29e64108c9997dbd937f517b92dfc6d8c70f54
Lance Buckley <erchancy@gmail.com>
2023-10-24 09:04:51 -0500

    Finished model and added fixture

--- kingdomdeathapi/models/achieved_milestone.py
+++ kingdomdeathapi/models/achieved_milestone.py
@@ -3,3 +3,4 @@ from django.db import models
 class AchievedMilestones(models.Model):
     settlement = models.ForeignKey("Settlement", on_delete=models.CASCADE, related_name="milestones")
     milestone = models.ForeignKey("Milestone", on_delete=models.CASCADE, related_name="achievements")
+    reached = models.BooleanField(default=False)
