cad8a8b9e07c6cef5d6aa8d97f960d907ad3c03b
Lance Buckley <erchancy@gmail.com>
2023-10-23 22:34:06 -0500

    Created achieved milestone model

---
+++ kingdomdeathapi/models/achieved_milestone.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class AchievedMilestones(models.Model):
+    settlement = models.ForeignKey("Settlement", on_delete=models.CASCADE, related_name="milestones")
+    milestone = models.ForeignKey("Milestone", on_delete=models.CASCADE, related_name="achievements")
