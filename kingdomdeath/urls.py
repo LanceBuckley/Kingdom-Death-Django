cebbf5cd93a129711c3199d2cc3911050011589c
Lance Buckley <erchancy@gmail.com>
2023-10-24 10:31:51 -0500

    Added __init__ to views and added paths for player views, login, and register in the urls.py

--- kingdomdeath/urls.py
+++ kingdomdeath/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from kingdomdeathapi.views import (login_user, register_user, PlayerView)
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'players', PlayerView, 'player')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
