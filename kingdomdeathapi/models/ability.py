5f785c8a060bfb63f3830157e7cbacc8d210495e
Lance Buckley <erchancy@gmail.com>
2024-03-28 17:11:40 -0500

    Added null=true to ability, disorder, event, fighting art, and weapon prof models. Added several new entries to the abilities fixture

--- kingdomdeathapi/models/ability.py
+++ kingdomdeathapi/models/ability.py
@@ -3,4 +3,4 @@ from django.db import models
 class Ability(models.Model):
     name = models.CharField(max_length=50)
     effect = models.CharField(max_length=50)
-    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_ability")
+    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_ability", null=True)
--- kingdomdeathapi/models/fighting_art.py
+++ kingdomdeathapi/models/fighting_art.py
@@ -3,4 +3,4 @@ from django.db import models
 class FightingArt(models.Model):
     name = models.CharField(max_length=50)
     effect = models.CharField(max_length=50)
-    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_fighting_art")
+    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_fighting_art", null=True)
