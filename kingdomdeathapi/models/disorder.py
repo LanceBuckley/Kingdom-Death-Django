52aef7a37575b49b381c82266ae4ca61c9c6e3ea
Lance Buckley <erchancy@gmail.com>
2024-03-28 17:58:03 -0500

    Added flavor_text to disorders, added several new entries to disorders fixtures. Increased character field length for effect

--- kingdomdeathapi/models/disorder.py
+++ kingdomdeathapi/models/disorder.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Disorder(models.Model):
     name = models.CharField(max_length=50)
+    flavor_text = models.CharField(max_length=50)
     effect = models.CharField(max_length=50)
     expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_disorder", null=True)
