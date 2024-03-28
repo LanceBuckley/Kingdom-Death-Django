83491d4a687701462f2d8daab38d10fe1ae38cef
Lance Buckley <erchancy@gmail.com>
2024-03-28 15:51:25 -0500

    Removed unnecessary admin file, changed ability, disorder, event, fighting art and weapon proficiency models to accomodate addition of expansion model

--- kingdomdeathapi/models/fighting_art.py
+++ kingdomdeathapi/models/fighting_art.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class FightingArt(models.Model):
     name = models.CharField(max_length=50)
+    effect = models.CharField(max_length=50)
+    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_fighting_art")
