19ff266353b58ca7b27f9eed63c84bfd0d9f04e0
Lance Buckley <erchancy@gmail.com>
2024-03-29 01:02:02 -0500

    Added vermin to resource_types. Added severa resources to resource fixture stopping at Dung Beetle Knight. Added consumable bool to resource model

--- kingdomdeathapi/models/resource.py
+++ kingdomdeathapi/models/resource.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Resource(models.Model):
     name = models.CharField(max_length=50)
-    type = models.ForeignKey("ResourceType", on_delete=models.CASCADE, related_name="applicable_resource")
+    type = models.ManyToManyField("ResourceType", related_name="applicable_resource")
+    consumable = models.BooleanField(default=False)
     monster = models.BooleanField(default=False)
     strange = models.BooleanField(default=False)
     indomitable = models.BooleanField(default=False)
     vermin = models.BooleanField(default=False)
     monster_origin = models.ForeignKey("Monster", on_delete=models.CASCADE, related_name="droppable_resource", null=True)
     expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_resource", null=True)
-    flavor_text = models.CharField(max_length=50)
-    effect = models.CharField(max_length=50)
+    flavor_text = models.CharField(max_length=50, null=True)
+    effect = models.CharField(max_length=50, null=True)
