f3d7a07d70114112c0cfd9cd469b7fd6f7a273ed
Lance Buckley <erchancy@gmail.com>
2024-03-28 23:37:12 -0500

    Added legendary bool field to monster model. Added several fields to resource model. Expanded monster fixture. Slightly expanded resource_type fixture. Began expanding resource fixture.

--- kingdomdeathapi/models/monster.py
+++ kingdomdeathapi/models/monster.py
@@ -4,4 +4,6 @@ class Monster(models.Model):
     name = models.CharField(max_length=50)
     expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_monster", null=True)
     nemesis = models.BooleanField(default=False)
+    core = models.BooleanField(default=False)
+    legendary = models.BooleanField(default=False)
     
--- kingdomdeathapi/models/resource.py
+++ kingdomdeathapi/models/resource.py
@@ -3,3 +3,11 @@ from django.db import models
 class Resource(models.Model):
     name = models.CharField(max_length=50)
     type = models.ForeignKey("ResourceType", on_delete=models.CASCADE, related_name="applicable_resource")
+    monster = models.BooleanField(default=False)
+    strange = models.BooleanField(default=False)
+    indomitable = models.BooleanField(default=False)
+    vermin = models.BooleanField(default=False)
+    monster_origin = models.ForeignKey("Monster", on_delete=models.CASCADE, related_name="droppable_resource", null=True)
+    expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_resource", null=True)
+    flavor_text = models.CharField(max_length=50)
+    effect = models.CharField(max_length=50)
