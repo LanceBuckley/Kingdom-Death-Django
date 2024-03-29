5d00dc5abab9ec0a65421a56e1ce840207979a76
Lance Buckley <erchancy@gmail.com>
2024-03-29 02:06:55 -0500

    Added several query filters to resource view. In order these are type_maps, bool_maps, monster_maps, and expansion_maps each of which filters by the associated fields. Removed depricated vermin field from resource model and view

--- kingdomdeathapi/models/resource.py
+++ kingdomdeathapi/models/resource.py
@@ -7,7 +7,6 @@ class Resource(models.Model):
     monster = models.BooleanField(default=False)
     strange = models.BooleanField(default=False)
     indomitable = models.BooleanField(default=False)
-    vermin = models.BooleanField(default=False)
     monster_origin = models.ForeignKey("Monster", on_delete=models.CASCADE, related_name="droppable_resource", null=True)
     expansion = models.ForeignKey("ExpansionType", on_delete=models.CASCADE, related_name="expansion_resource", null=True)
     flavor_text = models.CharField(max_length=50, null=True)
